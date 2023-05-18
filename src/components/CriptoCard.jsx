315ec17b9266e76e5453ff28d56cbec6a11de96a
mario arriola <riblarsv@gmail.com>
2023-05-18 04:37:28 +0200

    display basic data on screen

--- src/components/CriptoCard.jsx
+++ src/components/CriptoCard.jsx
@@ -2,7 +2,7 @@ import {useState,useEffect} from 'react'
 import axios from 'axios'
 function CryptoCard(){
 
-    const [cryptoData,setCryptoDat] = useState({})
+    const [cryptoData,setCryptoDat] = useState(null)
     const [cryptoToSearch,setCryptoToSearch] = useState('btc-bitcoin')
     async function callAPI(){
         const request = await axios.get(`https://api.coinpaprika.com/v1/coins/${cryptoToSearch}`)
@@ -13,6 +13,14 @@ function CryptoCard(){
         callAPI()
     },[cryptoToSearch])
    
-    return(<h1> here i will show the crypto information</h1>)
+    return(<>
+    {
+     cryptoData ?(<>
+     <h2>{cryptoData.name}</h2>
+     <p>{cryptoData.description}</p>
+     <img src={cryptoData.logo} alt={cryptoData.name}/>
+     </>) :"loading"
+    }
+    </>)
 }
 export default CryptoCard
