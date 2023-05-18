1ad6a5550997a90ca500dc7c11a568c10185cad0
sherniia <94130159+sherniia@users.noreply.github.com>
2023-05-17 21:43:22 -0500

    adding the first crypto card

--- src/components/CriptoCard.jsx
+++ src/components/CriptoCard.jsx
@@ -1,4 +1,28 @@
-function CryptoCard(){
-    return(<h1> here i will show the crypto information</h1>)
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function CryptoCard() {
+  const [cryptoData, setCryptoData] = useState({});
+  const [cryptoToSearch, setCryptoToSearch] = useState("btc-bitcoin");
+
+  async function callApi() {
+    const request = await axios.get(
+      `https://api.coinpaprika.com/v1/coins/${cryptoToSearch}`
+    );
+    setCryptoData(request.data);
+  }
+
+  useEffect(() => {
+    callApi();
+  }, [cryptoToSearch]);
+
+  return (
+    <>
+      <h1>{cryptoData.name}</h1>
+      <p>{cryptoData.description}</p>
+      <img src={cryptoData.logo} alt="" />
+
+    </>
+  );
 }
-export default CryptoCard
+export default CryptoCard;
