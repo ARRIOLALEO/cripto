c13e8cb97524733e026052fb5830809825953ded
mario arriola <riblarsv@gmail.com>
2023-05-18 04:16:11 +0200

    added the basic integration with the api

--- src/components/CriptoCard.jsx
+++ src/components/CriptoCard.jsx
@@ -1,4 +1,18 @@
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 function CryptoCard(){
+
+    const [cryptoData,setCryptoDat] = useState({})
+    const [cryptoToSearch,setCryptoToSearch] = useState('btc-bitcoin')
+    async function callAPI(){
+        const request = await axios.get(`https://api.coinpaprika.com/v1/coins/${cryptoToSearch}`)
+        setCryptoDat(request.data)
+    }
+
+    useEffect(()=>{
+        callAPI()
+    },[cryptoToSearch])
+   
     return(<h1> here i will show the crypto information</h1>)
 }
 export default CryptoCard
